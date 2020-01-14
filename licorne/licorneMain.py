a90f85b3b9df8563ab3280391181d1d54c9c8d02
georgeyumnam <georgeyumnam@gmail.com>
2020-01-14 09:09:00 -0500

    removed unnecessary files

--- licorne/licorneMain.py
+++ licorne/licorneMain.py
@@ -515,11 +515,11 @@ if __name__ == '__main__':
                                   sublayers=5,
                                   roughness=15.8909,
                                   roughness_model=RoughnessModel.TANH)
-    layer_3.msld.phi.vary = True
-    layer_4.msld.phi.vary = True
-    layer_5.msld.phi.vary = True
-    layer_7.msld.phi.vary = True
-    layer_8.msld.phi.vary = True
+    layer_3.msld.rho.vary = True
+    layer_4.msld.rho.vary = True
+    layer_5.msld.rho.vary = True
+    layer_7.msld.rho.vary = True
+    layer_8.msld.rho.vary = True
     smod.layers = [layer_1, layer_2, layer_3, layer_4, layer_5, layer_6, layer_7, layer_8]
     window = MainWindow(smod)
     window.show()
